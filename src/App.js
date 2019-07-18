82d1abc5fd5ecbe9ccaf7826cf1d7c043fb691c2
Kenneth Buan <buancneth@gmail.com>
2019-07-19 05:51:23 +0800

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 import './App.css';
 import Nav from './Components/Nav';
 import About from './Components/About';
@@ -8,6 +9,7 @@ import ItemDetail from './Components/ItemDetail';
 import UpcomingItems from './Components/UpcomingItems';
 import News from './Components/News';
 import Challenges from './Components/Challenges';
+
 
 function App() {
   return (
@@ -29,7 +31,44 @@ function App() {
 }
 
 const Home = () => (
-  <div>
+  <div className="home-slider">
+    <Carousel >
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://imgplaceholder.com/420x320"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://imgplaceholder.com/420x320"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://imgplaceholder.com/420x320"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
     <UpcomingItems/>
     
   </div>
